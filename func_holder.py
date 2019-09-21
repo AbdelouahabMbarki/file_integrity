83b9d8f2d0fa31e88ad3671130ebee8584e12e02
h1rkul2 <h1rkul2@test.com>
2019-09-21 17:19:51 +0100

    fixed the compare function and the bugs

--- func_holder.py
+++ func_holder.py
@@ -1,4 +1,4 @@
-import hashlib,os,files,time
+import hashlib,os,files,time,sys
 def md5(fname):
     try:
         hash_md5 = hashlib.md5()
@@ -7,8 +7,42 @@ def md5(fname):
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
     except OSError as e:
-        answer= fname + "  not found"
-        return answer
+        print(fname + "  not found")
+        sys.exit()
+
+def file_hash_exist_solve():
+    if not os.path.exists(md5file):
+         print("please run script again")
+         print("wait while creating 1 the config file for you ....")
+         time.sleep(2)
+         f= open(md5file,"w+")
+         f.close()
+         add_data_hashfile()
+
+    else :
+        compare_data(file1.pathname,file1.hash)
+        compare_data(file2.pathname,file2.hash)
+        compare_data(file3.pathname,file3.hash)
+        add_data_hashfile()
+
+def add_data_hashfile():
+    with open(md5file,"w") as file:
+        file.writelines(file1.pathname+":"+file1.hash+"\n")
+        file.writelines(file2.pathname+":"+file2.hash+"\n")
+        file.writelines(file3.pathname+":"+file3.hash+"\n")
+    file.close()
+
+def compare_data(fname,hash):
+    with open(md5file,"r") as file:
+        readedfile=file.readlines()
+    for element in range(len(readedfile)):
+        spar=readedfile[element].split(":")
+        if (spar[0] ==fname):
+            if (spar[1].rstrip("\n")==hash):
+                return
+            else :
+                print(fname+" is  modified")
+                return
 
 file1=files.file("/tmp/file1.txt")
 file2=files.file("/tmp/file2.txt")
@@ -20,36 +54,4 @@ file3.hash=md5(file3.pathname)
 
 md5file="/tmp/.md5file"
 
-def file_hash_exist_solve():
-    if not os.path.exists(md5file):
-         print("it doesn't exist")
-         print("creating 1 for you ....")
-         time.sleep(2)
-         f= open(md5file,"w+")
-         f.close()
-         add_data_hashfile()
-
-    else :
-        print("it exists ")
-        add_data_hashfile()
-
-def add_data_hashfile():
-    with open(md5file,"w") as file:
-        file.writelines(file1.pathname+":"+file1.hash+"\n")
-        file.writelines(file2.pathname+":"+file2.hash+"\n")
-        file.writelines(file3.pathname+":"+file3.hash+"\n")
-    file.close()
-
-def compare_data():
-    with open(md5file,"r") as file:
-        x=file.readlines()
-    for element in range(len(x)):
-        z=x[element].split(":")
-        if (z[1].rstrip("\n")==file1.hash):
-            print("yes file1 is the same")
-            return
-        if (z[1].rstrip("\n")==file1.hash):
-            print("yes file1 is the same")
-    print(z)
 file_hash_exist_solve()
-compare_data()
